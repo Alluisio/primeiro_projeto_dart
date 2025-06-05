1c71b1ec528706122963960fcc73ecf318481256
Alluisio Silva <alluisio.silva@ipdec.org>
2025-06-05 10:54:10 -0400

    feat: using dart:io, variables and interpolation

--- bin/primeiro_projeto_dart.dart
+++ bin/primeiro_projeto_dart.dart
@@ -1,3 +1,11 @@
+import 'dart:io';
+
 void main() {
   print('project clean');
+
+  var entrada = stdin.readLineSync();
+
+  const saida = 'ei ta porra meno';
+
+  print('entrada: $entrada $saida');
 }
